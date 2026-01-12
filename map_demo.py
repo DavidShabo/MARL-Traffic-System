88e51b4bc6559d03a6c34269d186c049cf0788e5
David-Fr-afk <dvdshabo@gmail.com>
2026-01-12 12:13:38 -0500

    3d version

--- map_demo.py
+++ map_demo.py
@@ -1,42 +1,21 @@
-import matplotlib.pyplot as plt
-from metadrive import (
-    MultiAgentMetaDrive,
-    MultiAgentTollgateEnv,
-    MultiAgentBottleneckEnv,
-    MultiAgentIntersectionEnv,
-    MultiAgentRoundaboutEnv,
-    MultiAgentParkingLotEnv
-)
+from metadrive import MultiAgentRoundaboutEnv
 
-env_classes = dict(
-    roundabout=MultiAgentRoundaboutEnv,
-    intersection=MultiAgentIntersectionEnv,
-    tollgate=MultiAgentTollgateEnv,
-    bottleneck=MultiAgentBottleneckEnv,
-    parkinglot=MultiAgentParkingLotEnv,
-    pgma=MultiAgentMetaDrive
-)
+env = MultiAgentRoundaboutEnv({
+    "use_render": True,
+    "manual_control": False,
+    "log_level": 50,
+    "num_agents": 8,
+})
 
-fig, axs = plt.subplots(2, 3, figsize=(10, 6.5), dpi=200)
-plt.tight_layout(pad=-3)
+obs, info = env.reset(seed=0)
 
-for i in range(2):
-    for j in range(3):
-        env = list(env_classes.values())[i*3+j]({"log_level":50})
-        env.reset(seed=0)
-        m = env.render(mode="topdown", 
-                       # get the overview of the scene
-                       film_size = (1000, 1000),
-                       screen_size = (1000, 1000),
-                       # set camer to map center
-                       camera_position=env.current_map.get_center_point(), 
-                       # auto determine the number of pixels for 1 meter 
-                       scaling=None,
-                       # do not pop window
-                       window=False)
-        ax = axs[i][j]
-        ax.imshow(m, cmap="bone")
-        ax.set_xticks([])
-        ax.set_yticks([])
-        env.close()
-plt.show()
+try:
+    while True:
+        env.render(mode="human")              # 3D window
+        actions = env.action_space.sample()   # correct multi-agent action dict
+        obs, rew, term, trunc, info = env.step(actions)
+
+        if term.get("__all__", False) or trunc.get("__all__", False):
+            obs, info = env.reset()
+finally:
+    env.close()
